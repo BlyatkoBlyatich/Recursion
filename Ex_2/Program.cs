cf6c4932690be3bfbfec8b92cc5683c137afbe5d
Polovatel <polovatel@yandex.ru>
2022-12-05 16:55:47 +0300

    Ex 2 done

---
+++ Ex_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int FindSum(int m, int n)
+{
+    int sum = 0;
+    if(m <= n)
+    {
+        sum += m + FindSum(m + 1, n);
+        return sum;
+    }
+    return sum;
+}
+Console.WriteLine(FindSum(2, 6));
